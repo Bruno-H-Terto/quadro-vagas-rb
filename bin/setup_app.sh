626e6c7681af1593e68c11c871dd95e6401b7a95
Bruno Herculano <herculanoterto@gmail.com>
2025-03-04 17:43:44 -0300

    fix: Fix permissions on remote server to run tests in Docker container

--- bin/setup_app.sh
+++ bin/setup_app.sh
@@ -12,6 +12,10 @@ fi
 
 bin/rails db:migrate
 
+if [[ -f /home/app/web/log/test.log ]]; then
+  sudo chmod 0664 /home/app/web/log/test.log
+fi
+
 current_date=$(date "+%Y-%m-%d %H:%M:%S")
 if ! [[ -f ./docker/development/tmp/.db-seeded ]]; then
   bin/rails db:seed && echo "Seeded database in $current_date UTC" >> ./docker/development/tmp/.db-seeded
